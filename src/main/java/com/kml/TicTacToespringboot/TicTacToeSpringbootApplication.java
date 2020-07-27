c9d4cf15fe5167c7774e551eb454c4fe1c8a06af
Kamil Osak <32075064+kmlhombre@users.noreply.github.com>
2020-07-27 14:44:30 +0200

    Frist commit

---
+++ src/main/java/com/kml/TicTacToespringboot/TicTacToeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.kml.TicTacToespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToeSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kml/TicTacToespringboot/TicTacToeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kml.TicTacToespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
